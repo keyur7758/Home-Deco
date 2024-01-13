c0068702d03a32fe5b57628b2b167b28f851634d
keyur7758 <keyurk397@gmail.com>
2024-01-13 16:27:23 +0530

    Menubar Modified

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,31 +2,32 @@ import React, { useEffect, useState } from 'react'
 import './Header.css'
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
+import Canva from '../offCanva/Canva';
 
 export default function Header() {
-  const [nav, setNav] = useState(false)
-  const [isActive, setIsActive] = useState(false);
+    const [nav, setNav] = useState(false)
+    const [isActive, setIsActive] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      // Clean up the event listener on component unmount
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [])
+    useEffect(() => {
+        const handleScroll = () => {
+            window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            // Clean up the event listener on component unmount
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [])
 
-  const headerStyles = {
-    position: 'fixed',
-    width: '100%',
-    zIndex: 10,
-    transition: 'all',
-    transition: "ease-out 0.3s",
-    backgroundColor: isActive ? '#ffffff' : 'transparent',
-    boxShadow: isActive ? '0 2px 4px rgba(0, 0, 0, 0.1)' : 'none',
-  };
+    const headerStyles = {
+        position: 'fixed',
+        width: '100%',
+        zIndex: 10,
+        transition: 'all',
+        transition: "ease-out 0.3s",
+        backgroundColor: isActive ? '#ffffff' : 'transparent',
+        boxShadow: isActive ? '0 2px 4px rgba(0, 0, 0, 0.1)' : 'none',
+    };
 
     return (
         <>
@@ -34,7 +35,7 @@ export default function Header() {
                 <div className='row g-0'>
                     <div className='col-md-6 col-6'>
                         <div className='header-box'>
-                            <h2 className='logo'><Link to="/"><img className='img-fluid home-logo' src='/images/Home-Logo.png'/></Link></h2>
+                            <h2 className='logo'><Link to="/"><img className='img-fluid home-logo' src='/images/Home-Logo.png' /></Link></h2>
                         </div>
                     </div>
                     <div className='col-md-6 col-6'>
@@ -44,58 +45,14 @@ export default function Header() {
                                 <li><Link to="/about">ABOUT</Link></li>
                                 <li><Link to="/contact">CONTACT</Link></li>
                             </ul>
-                            <i class="fa-solid fa-bars-staggered" onClick={() => setNav(true)}></i>
+                            {/* <i class="fa-solid fa-bars-staggered" onClick={() => setNav(true)}></i> */}
+                            {['end'].map((end, idx) => (
+                                <Canva key={idx} placement={end}  />
+                            ))}
                         </div>
                     </div>
                 </div>
             </div>
-            {
-                nav &&
-                <div className='menu-area'>
-                    <div className='row g-0'>
-                        <div className='col-md-6'>
-                            <div className='menu-box'>
-
-                            </div>
-                        </div>
-                        <div className='col-md-6 text-end'>
-                            <i class="fa-solid fa-xmark" onClick={() => setNav(false)}></i>
-                            <div className='menu-box'>
-                            <Link to="/contact"><Button variant="outline-dark" className="book-btn">MAKE A BOOKING</Button></Link>
-                                <ul>
-                                    <li><Link to="/">HOME</Link></li>
-                                    <li><Link to="/about">ABOUT</Link></li>
-                                    <li><Link to="/contact">CONTACT</Link></li>
-                                </ul>
-                                <p className='line'></p>
-                                <ul>
-                                    <li>lorem ipsum</li>
-                                    <li>lorem ipsum</li>
-                                    <li>lorem ipsum</li>
-                                    <li>lorem ipsum</li>
-                                    <li>lorem ipsum</li>
-                                    <li>lorem ipsum</li>
-                                    <li>lorem ipsum</li>
-                                    <li>lorem ipsum</li>
-                                    <li>lorem ipsum</li>
-                                    <li>lorem ipsum</li>
-                                    <li>lorem ipsum</li>
-                                    <li>lorem ipsum</li>
-                                    <li>lorem ipsum</li>
-                                    <li>lorem ipsum</li>
-                                </ul>
-                                <p className='line'></p>
-                                <ul>
-                                    <li><i class="fa-brands fa-facebook"></i>facebook</li>
-                                    <li><i class="fa-brands fa-instagram"></i>instagram</li>
-                                    <li><i class="fa-brands fa-twitter" ></i>tweeter</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            }
-
         </>
     )
 }
